b7695033e06a87319577f567ffd1e7caaa9dda0a
tarunsuresh06 <tarunsuresh.v@gmail.com>
2024-06-17 14:27:18 +0530

    Initial Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-full">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
