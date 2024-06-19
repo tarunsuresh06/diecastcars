22da499959a18063a11aa1977fcecfb5e6512696
tarunsuresh06 <tarunsuresh.v@gmail.com>
2024-06-20 00:09:07 +0530

    Add Hero Section carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,16 @@
+import HeroImageSlider from "@/components/HeroImageSlider";
+
 export default function Home() {
-  return <div>Home</div>;
+  let slides = [
+    "https://opencart.mahardhi.com/MT04/fantastic/image/cache/catalog/banners/mainbanner1-1920x600.jpg",
+    "https://opencart.mahardhi.com/MT04/fantastic/image/cache/catalog/banners/mainbanner3-1920x600.jpg",
+    "https://opencart.mahardhi.com/MT04/fantastic/image/cache/catalog/banners/mainbanner2-1920x600.jpg",
+  ];
+  return (
+    <main>
+      <div>
+        <HeroImageSlider slides={slides} />
+      </div>
+    </main>
+  );
 }
