914d734039dd2f98f938b15746da8996c00cca54
tarunsuresh06 <tarunsuresh.v@gmail.com>
2024-06-22 00:32:16 +0530

    Add Banner Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
     <>
       <HeroImageSlider slides={slides} />
       <div className="wrapper">
-        <section id="#newProducts">
-          <h1 className="text-4xl font-semibold mb-5">New Products</h1>
+        <section id="#featuredProductSection">
+          <h1 className="text-4xl font-semibold mb-5">Featured Products</h1>
           <Carousel>
             <CarouselContent className="pb-4">
               <CarouselItem className="basis-[80%] md:basis-1/2 lg:basis-1/3">
@@ -44,13 +44,54 @@ export default function Home() {
           </Carousel>
         </section>
       </div>
-      <section id="testimonials">
+      <section id="testimonialSection">
         <div className="bg-center bg-cover bg-fixed bg-[url('https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg')] h-[500px] ">
           <div className="wrapper flex justify-center items-center h-full">
             <Testimonial />
           </div>
         </div>
       </section>
+      <div className="wrapper">
+        <section id="#newProductSection">
+          <h1 className="text-4xl font-semibold mb-5">New Products</h1>
+          <Carousel>
+            <CarouselContent className="pb-4">
+              <CarouselItem className="basis-[80%] md:basis-1/2 lg:basis-1/3">
+                <ProductCard />
+              </CarouselItem>
+
+              <CarouselItem className="basis-[80%] md:basis-1/2 lg:basis-1/3">
+                <ProductCard />
+              </CarouselItem>
+
+              <CarouselItem className="basis-[80%] md:basis-1/2 lg:basis-1/3">
+                <ProductCard />
+              </CarouselItem>
+
+              <CarouselItem className="basis-[80%] md:basis-1/2 lg:basis-1/3">
+                <ProductCard />
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious className="hidden lg:inline-flex" />
+            <CarouselNext className="hidden lg:inline-flex" />
+          </Carousel>
+        </section>
+        <section id="bannnerSection">
+          <div className="flex flex-col md:flex-row justify-between items-center gap-4 my-14">
+            <img
+              className="w-full md:w-[50%] max-w-[600px]"
+              src="https://opencart.mahardhi.com/MT04/fantastic/image/catalog/banners/banner1.jpg"
+              alt="banner 1"
+            />
+
+            <img
+              className="w-full md:w-[50%] max-w-[600px]"
+              src="https://opencart.mahardhi.com/MT04/fantastic/image/catalog/banners/banner2.jpg"
+              alt="banner 2"
+            />
+          </div>
+        </section>
+      </div>
     </>
   );
 }
