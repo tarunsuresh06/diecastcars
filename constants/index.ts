3d5f2ee9716ab2ad8965a6822bfa44c49d751930
Tarun Suresh <tarunsuresh.v@gmail.com>
2024-06-25 15:55:35 +0530

    Add BrandsLinks List in Constant

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const brandsLinks = [
+    {
+        name: "Hot Wheels",
+        path: "brands/hot-wheels"
+    },
+    {
+        name: "Barbie",
+        path: "brands/barbie"
+    },
+    {
+        name: "Win Magic",
+        path: "brands/win-magic"
+    },
+    {
+        name: "Paw Patrol",
+        path: "brands/paw-patrol"
+    },
+    {
+        name: "Topps",
+        path: "brands/topps"
+    },
+]
